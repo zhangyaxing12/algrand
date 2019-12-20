6926d711b11c91372852d971950821d588fee6a9
Marshall Abrams <marshall@logical.net>
2019-12-20 11:46:05 -0600

    added functions for exploring pair-encoding

--- src/algrand/core.clj
+++ src/algrand/core.clj
@@ -1,6 +1,27 @@
-(ns algrand.core)
+(ns algrand.core
+    (:require [clojure.pprint :as pp]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;; Example usage:
+;; (run! fmt-encoding (sort (drop-take 1000000 40 encodings)))
+
+(defn drop-take
+  "Drop, then take."
+  [to-drop to-take coll]
+  (take to-take (drop to-drop coll)))
+
+(defn encode-pair
+  "Single-number encoding of pairs from Nies' *Computability and Randomness*
+  (almost identical to the one in Li and Vitanyis' *An Introduction to Kolmogorov 
+  Complexity and Its Applications)."
+  [m n]
+  (+ m 
+     (/ (* (+ m n) (+ m n 1)) 2)))
+
+(def encodings (for [x (range 10000) y (range 1000)]
+                    [(encode-pair x y) x y]))
+
+(defn fmt-encoding 
+  "Given a sequence containing an encoding followed by its two elements,
+  in order, print binary reps of each to stdout."
+  [[e x y]]
+  (pp/cl-format true "~16b ~16b ~16b~%" e x y))
