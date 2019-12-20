a9311e22d2df6017194f40d985a2a4b033f651e3
Marshall Abrams <marshall@logical.net>
2019-12-20 11:49:43 -0600

    updated license, description, clojure version

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject algrand "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+(defproject algrand "0.1.0"
+  :description "Code for thinking about algorithmic randomness"
+  :url "ttps://github.com/mars0i/algrand"
+  :license {:name "Gnu General Public License version 3.0"
+            :url "http://www.gnu.org/copyleft/gpl.html"}
+  :dependencies [[org.clojure/clojure "1.10.1"]]
   :repl-options {:init-ns algrand.core})
