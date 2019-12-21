b25d6bcf0ef0ef9516bfaaf0803e89606454a4ec
Marshall Abrams <marshall@logical.net>
2019-12-21 12:35:35 -0600

    added better version of count-bits, added natural-number?

--- src/algrand/core.clj
+++ src/algrand/core.clj
@@ -60,10 +60,38 @@
   [[e x y]]
   (pp/cl-format true "~20b ~20b ~20b~%" e x y))
 
+(defn natural-number?
+  "True iff n is a non-negative integer."
+  [n] 
+  (and (integer? n) (>= n 0)))
+
+;; bit-counting done "right"; helper function
+(defn count-bits-once
+  "Count the number of bits in non-negative integer n."
+  [n]
+  (when (natural-number? n)
+    (cond (zero? n) 1              ; special case
+          :else (loop [x n         ; positive integers
+                       bits 0] 
+                      (if (< x 1) 
+                        bits 
+                        (recur (* x 0.5) (inc bits)))))))
+                               ;; floats faster, space-efficent vs rationals
+
+;; bit-counting done "right"
 (defn count-bits
+  "Count the number of bits in non-negative integers ns."
+  [& ns]
+  (reduce (fn [bits n] (+ bits (count-bits-once n)))
+          0 ns))
+
+
+;; simple hacky version--a little slower than "right" version
+;; note toBinaryString can't handle bigints, so use cl-format
+(defn cheezy-count-bits
   "Count the number of bits in non-negative integers ns."
   [& ns]
   (count 
     (apply str 
-           (map (fn [n] (Integer/toBinaryString n))
+           (map (fn [n] (pp/cl-format nil "~b" n)) ; (Integer/toBinaryString n)
                 ns))))
