a0e16bc8b372c8e745444d0906ce216108853081
Marshall Abrams <marshall@logical.net>
2019-12-20 22:43:39 -0600

    added numeric-tower

--- project.clj
+++ project.clj
@@ -3,5 +3,7 @@
   :url "ttps://github.com/mars0i/algrand"
   :license {:name "Gnu General Public License version 3.0"
             :url "http://www.gnu.org/copyleft/gpl.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/math.numeric-tower "0.0.4"]
+                ]
   :repl-options {:init-ns algrand.core})
