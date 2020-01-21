bd2df4be73d59d533f0737f99439abd893e9d4ed
Marshall Abrams <marshall@logical.net>
2020-01-21 10:29:47 -0600

    starting to implement Nies' Kraft-Chaitin procedure

---
+++ src/clj/algrand/kraftchaitin.clj
@@ -0,0 +1,25 @@
+(ns algrand.kraftchaitin
+    (:require [clojure.pprint :as pp]))
+
+;; Nie's proof of the Machine Existence (Kraft-Chaitin) theorem:
+
+(defn max-shorter
+  "Given a sequence of strings Rn-1 sorted by length, and a string size rn,
+  returns the largest string with size greater than or equal to rn."
+  [Rn-1 rn]
+  (loop [rs Rn-1]
+    (when rs  ; if this is nil, bad args--we went too far
+      (let [next-rs (next rs)
+            r (first next-rs)
+            r-size (count r)]
+        (cond
+          (= r-size rn) r            ; equal size, so choose this one
+          (> r-size rn) (first rs)   ; too big--go back to preceding one
+          :else (recur next-rs)))))) ; keep looking
+
+;; assume Rn sorted by length
+;(defn blah
+;  [Rn-1 rn wn]
+;  (
+
+
