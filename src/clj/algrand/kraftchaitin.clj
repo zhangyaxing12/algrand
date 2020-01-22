8b988955d7d378f0165b60522c19e722fbe1f4ba
Marshall Abrams <marshall@logical.net>
2020-01-21 22:54:59 -0600

    added several new fns as components of new stage
    Final packaging of new stage is still in progress.

--- src/clj/algrand/kraftchaitin.clj
+++ src/clj/algrand/kraftchaitin.clj
@@ -9,7 +9,7 @@
 (defn max-shorter
   "Given a sequence of strings Rn-1 sorted by length, and a string size r,
   returns the largest string with size greater than or equal to r."
-  [Rn-1 r]
+  [r Rn-1]
   (loop [zs Rn-1]
     (when zs  ; if this is nil, bad args--we went too far
       (let [next-zs (next zs)
@@ -18,9 +18,37 @@
           (first zs)  ; too big--go back to preceding one
           (recur next-zs))))))
 
-;; assume Rn sorted by length
-;(defn blah
-;  [Rn-1 rn wn]
-;  (
+;  (let [z (max-shorter Rn-1 r)])
+        ;pad-len (- r (count z))]
 
+(defn make-w
+  [old-z pad-len]
+  (apply str 
+         old-z 
+         (repeat pad-len \0))) ; returns z if pad-len is zero
 
+(defn make-new-z
+  [old-z pad-len]
+  (if (< pad-len 1)
+    nil ; this shouldn't happen
+    (str
+      (apply str 
+             old-z
+             (repeat (dec pad-len) \0))
+      \1)))
+
+(defn make-zs
+  [old-z max-pad-len]
+  (map (fn [pad-len] (make-new-z old-z pad-len))
+       (range 1 (inc max-pad-len))))
+
+;; TODO  remove old z from Rn-1 (maybe do that in max-shorter?)
+;;       and append the new-zs to it,
+;;       then return it with w. or something
+(defn next-R-stage
+  [Rn-1 r]
+  (let [z (max-shorter r Rn-1)
+        pad-len (- r (count z))
+        w (make-w z pad-len) 
+        new-zs (make-zs z pad-len)]
+    ))
