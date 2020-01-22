46c3a21defeb7440d900c2118a2203ac3aaba7d8
Marshall Abrams <marshall@logical.net>
2020-01-21 21:59:15 -0600

    minor cleanup in code

--- src/clj/algrand/kraftchaitin.clj
+++ src/clj/algrand/kraftchaitin.clj
@@ -3,17 +3,20 @@
 
 ;; Nie's proof of the Machine Existence (Kraft-Chaitin) theorem:
 
+;; Note that when r = length of z, that's the z we want.  This
+;; function will skip past it and pull it back on the next iteration.
+;; This very slight inefficiency makes the code simpler.
 (defn max-shorter
-  "Given a sequence of strings Rn-1 sorted by length, and a string size rn,
-  returns the largest string with size greater than or equal to rn."
-  [Rn-1 rn]
-  (loop [rs Rn-1]
-    (when rs  ; if this is nil, bad args--we went too far
-      (let [next-rs (next rs)
-            r-size (count (first next-rs))]
-        (if (> r-size rn)
-          (first rs)   ; too big--go back to preceding one
-          (recur next-rs)))))) ; keep looking
+  "Given a sequence of strings Rn-1 sorted by length, and a string size r,
+  returns the largest string with size greater than or equal to r."
+  [Rn-1 r]
+  (loop [zs Rn-1]
+    (when zs  ; if this is nil, bad args--we went too far
+      (let [next-zs (next zs)
+            z-size (count (first next-zs))]
+        (if (> z-size r)
+          (first zs)  ; too big--go back to preceding one
+          (recur next-zs))))))
 
 ;; assume Rn sorted by length
 ;(defn blah
